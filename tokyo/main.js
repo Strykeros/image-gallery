4370281128901897250ffe4a6a77f3aa3d8ca695
Strykeros <Strykeros-PC>
2021-04-04 23:42:54 +0300

    working tokyo page

--- tokyo/main.js
+++ tokyo/main.js
@@ -6,6 +6,8 @@ var images = document.getElementsByClassName("mySlides");
 var modalImg = document.getElementById("tokyoimg");
 //var captionText = document.getElementById("caption");
 var slides = document.getElementsByClassName("mySlides2");
+var slideImage = document.querySelectorAll(".mySlides2 img");
+var column = document.querySelectorAll(".cursor");
 
 // Go through all of the images with our custom class
 for (var i = 0; i < images.length; i++) {
@@ -14,10 +16,12 @@ for (var i = 0; i < images.length; i++) {
   // and attach our click listener for this image.
   img.onclick = function(evt) {
     modal.style.display = "block";
-    modalImg.src = this.src;
   }
 }
 
+for(var i = 0; i < slideImage.length; i++){
+  slideImage[i].classList.add("fade");
+}
 
 
 // Get the <span> element that closes the modal
@@ -30,18 +34,20 @@ span.onclick = function() {
 
 // slideshow 
 
-
-
 showSlides(slideIndex);
 
 function plusSlides(n) {
   showSlides(slideIndex += n);
+
 }
 
 function currentSlide(n) {
   showSlides(slideIndex = n);
 }
 
+function selectedColumnSlide(n) {
+  showSlides(slideIndex = n);
+}
 
 function showSlides(n) {
   var i;
@@ -52,24 +58,12 @@ function showSlides(n) {
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
       slides[i].style.display = "none";
-
   }
   for (i = 0; i < dots.length; i++) {
       dots[i].className = dots[i].className.replace(" active", "");  
   }
-  slides[slideIndex-1].style.display = "block";
+  slides[slideIndex-1].style.display = "block";  
   dots[slideIndex-1].className += " active";
   //captionText.innerHTML = dots[slideIndex-1].alt;
 }
-
-let imgArray = [
-  "tokyoimg1.jpg",  "tokyoimg2.jpg",  "tokyoimg3.jpg",  "tokyoimg4.jpg",  "tokyoimg5.jpg",  "tokyoimg6.jpg", 
-  "tokyoimg7.jpg",  "tokyoimg8.jpg",  "tokyoimg9.jpg",  "tokyoimg10.jpg",  "tokyoimg11.jpg",  "tokyoimg12.jpg"
-];
-
-let Jsonimg = {
-  "image1": "tokyoimg1.jpg",
-  "image2": "tokyoimg2.jpg",
-  "image3": "tokyoimg3.jpg"
-};
 
